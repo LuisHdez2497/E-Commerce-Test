4b096d8a56a456d251e844905531d961bb45db72
Luis Alfonso Hernandez <luisalfonso.hnunez@gmail.com>
2022-09-02 17:07:31 -0600

    Login redirect

--- resources/src/components/Header.vue
+++ resources/src/components/Header.vue
@@ -4,10 +4,10 @@
             <div class="px-6 py-9">
                 <div class="container mx-auto flex items-end justify-end">
                     <div class="justify-end flex items-center space-x-4">
-                        <div class="flex items-end space-x-4 xl:space-x-8">
+                        <div class="flex items-end space-x-8 xl:space-x-8">
                             <router-link to="/cart"
-                                    class="text-black text-xl font-bold hover:text-orange-500">
-                                <svg class="fill-stroke" width="40" height="40" viewBox="0 0 26 26" fill="none"
+                                    class="text-black text-xl text-center font-bold hover:text-orange-500">
+                                <svg class="fill-stroke" width="45" height="45" viewBox="0 0 26 26" fill="none"
                                      xmlns="http://www.w3.org/2000/svg">
                                     <path
                                         d="M5 1L1 5.8V22.6C1 23.2365 1.28095 23.847 1.78105 24.2971C2.28115 24.7471 2.95942 25 3.66667 25H22.3333C23.0406 25 23.7189 24.7471 24.219 24.2971C24.719 23.847 25 23.2365 25 22.6V5.8L21 1H5Z"
@@ -21,6 +21,7 @@
                                         stroke-linejoin="round"/>
                                 </svg> Cart
                             </router-link>
+                            <router-link to="/login" class="text-black text-xl font-bold hover:text-orange-500"><i class="mdi mdi-login-variant block text-5xl text-center"></i>Login</router-link>
                         </div>
                     </div>
                 </div>
